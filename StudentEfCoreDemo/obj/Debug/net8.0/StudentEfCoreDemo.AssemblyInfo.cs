b8bb949b8003b90e588cb6a3d1daf16604647f52
Martin <martin.nipuli2@gmail.com>
2025-02-14 09:47:51 +0200

    Add CI pipeline configuration

--- StudentEfCoreDemo/obj/Debug/net8.0/StudentEfCoreDemo.AssemblyInfo.cs
+++ StudentEfCoreDemo/obj/Debug/net8.0/StudentEfCoreDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentEfCoreDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9960ed6cf43a74f3386699e82ee306a62009f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentEfCoreDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentEfCoreDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TestProject1/obj/Release/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Release/net8.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb02f263e200fcadcb387723906fda427de695c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9960ed6cf43a74f3386699e82ee306a62009f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
